416bc92aa319df5174f2847a35859a277a3485c8
Md. Zarif Muntakim <78073923+muntakimzarif@users.noreply.github.com>
2025-08-02 04:15:15 +0600

    Update 03_Instance Method and Method Overloading.java

--- 03_Instance Method and Method Overloading.java
+++ 03_Instance Method and Method Overloading.java
@@ -13,8 +13,41 @@ public class BankAccount{
   }
 }
 
+public class BankAccountTester{
+  public static void main(String args[]){
+     BankAccount acc1 = new BankAccount();
+     System.out.println(acc1.printDetails());
+     System.out.println("-----1-----");
+     acc1.setInfo(1456890,"Salary");
+     System.out.println("-----2-----");
+     System.out.println(acc1.printDetails());
+     System.out.println("-----3-----");
+     BankAccount acc2  = new BankAccount();
+     acc2.setInfo(1765498,"Student");
+     System.out.println("-----4-----");
+     System.out.println(acc2.printDetails());
+  }
+}
+
 
 2)
+public class ShapeTester{
+  public static void main(String args []){
+    Shape circle = new Shape();
+    Shape triangle = new Shape();
+    Shape rectangle = new Shape();
+    
+    circle.setParameters("Circle", 5);
+    triangle.setParameters("Triangle", 4, 7);
+    rectangle.setParameters("Rectangle", 2.4, 4.4);
+    
+    System.out.println(circle.details());
+    System.out.println("1---------------");
+    System.out.println(triangle.details());
+    System.out.println("2---------------");
+    System.out.println(rectangle.details());  
+  }
+}
 
 public class Shape{
   public String name;
@@ -44,6 +77,26 @@ public class Shape{
 
 
 3)
+public class ShelfTester{
+  public static void main(String [] args){
+    Shelf shelf = new Shelf();
+    shelf.showDetails();
+    System.out.println("1---------------");
+    shelf.addBooks(3);
+    System.out.println("2---------------");
+    shelf.capacity = 7;
+    shelf.addBooks(3);
+    System.out.println("3---------------");
+    shelf.showDetails();
+    System.out.println("4---------------");
+    shelf.addBooks(5);
+    shelf.showDetails();
+    shelf.capacity += 4;
+    System.out.println("6---------------");
+    shelf.addBooks(5);
+    shelf.showDetails();
+  }
+}
 
 public class Shelf{
   public int capacity = 0;
@@ -68,6 +121,31 @@ public class Shelf{
 
 
 4)
+public class StudentTester{
+  public static void main(String[] args) {
+    Student std1 = new Student();
+    std1.showDetails();
+    System.out.println("1---------------");
+    std1.updateDetails("Alif", 3.99, 12);
+    System.out.println("2---------------");
+    std1.checkScholarshipEligibility();
+    System.out.println("3---------------");
+    std1.showDetails();
+    Student std2 = new Student();
+    std2.updateDetails("Mim", 3.4);
+    Student std3 = new Student();
+    std3.updateDetails("Henry", 3.5, 15, "BBA");
+    System.out.println("5---------------");
+    std2.checkScholarshipEligibility();
+    System.out.println("6---------------");
+    std3.checkScholarshipEligibility();
+    System.out.println("7---------------");
+    std2.showDetails();
+    System.out.println("8---------------");
+    std3.showDetails();
+  }
+}
+
 
 public class Student{
   public String name = "Not Set";
@@ -118,6 +196,35 @@ public class Student{
 }
 
 5)
+public class Tester5{
+  public static void main(String[] args) {
+    Library a1 = new Library();
+    a1.setBookCapacity(3);
+    System.out.println("1-------------");
+    a1.addBook("Ice");
+    System.out.println("2-------------");
+    a1.printDetail();
+    System.out.println("3-------------");
+    a1.addBook("Emma");
+    a1.addBook("Wings");
+    a1.addBook("Next");
+    System.out.println("4-------------");
+    a1.printDetail();
+    Library a2 = new Library();
+    a2.setBookCapacity(4);
+    System.out.println("5-------------");
+    a2.addBook("Onnobhubon");
+    a2.addBook("Ami");
+    System.out.println("6-------------");
+    a2.printDetail();
+    System.out.println("7-------------");
+    a2.addBook("Deyal");
+    a2.addBook("Himu");
+    a2.addBook("Megher Upor Bari");
+    System.out.println("8-------------");
+    a2.printDetail();
+  }
+}
 
 public class Library{
   public int capacity = 0;
@@ -149,6 +256,33 @@ public class Library{
 
  
 6)
+public class TaxiTester{
+ public static void main(String[] args) {
+   TaxiLagbe taxi1 = new TaxiLagbe();
+   taxi1.storeInfo("1010-01", "Dhaka");
+   System.out.println("1-------------");
+   taxi1.printDetails();
+   System.out.println("2-------------");
+   taxi1.addPassenger("Wilson", 105);
+   System.out.println("3-------------");
+   taxi1.printDetails();
+   System.out.println("4-------------");
+   taxi1.addPassenger("Walker", 100, "Wood", 200);
+   System.out.println("5-------------");
+   taxi1.printDetails();
+   System.out.println("6-------------");
+   taxi1.addPassenger("Karen", 200);
+   taxi1.addPassenger("Donald", 130);
+   System.out.println("7-------------");
+   taxi1.printDetails();
+   System.out.println("8-------------");
+   TaxiLagbe taxi2 = new TaxiLagbe();
+   taxi2.storeInfo("1010-02", "Khulna");
+   taxi2.addPassenger("Don", 115, "Parker", 215);
+   System.out.println("9-------------");
+   taxi2.printDetails();
+ }
+}
 
 public class TaxiLagbe{
   public String tNumber;
@@ -200,6 +334,43 @@ public class TaxiLagbe{
 
 
 7)
+public class CartTester{
+  public static void main(String [] args){
+    Cart c1 = new Cart (); 
+    Cart c2 = new Cart ();
+    Cart c3 = new Cart ();
+    
+    c1.create_cart(1);
+    c2.create_cart(2);
+    c3.create_cart(3);
+    System.out.println("====1====");
+    c1.addItem("Table", 3900.5);
+    c1.addItem("Chair", 1400.76);
+    c1.addItem(5400.87, "Television");
+    c1.addItem(5000.0, "Refrigerator");
+    
+    System.out.println("====2====");
+    c2.addItem("Stove",439.90);
+    
+    System.out.println("====3====");
+    c3.addItem("Chair",1400.5);
+    c3.addItem(3400.0, "Chair");
+    
+    System.out.println("====4====");
+    c1.cartDetails();
+    
+    System.out.println("====5====");
+    c2.cartDetails();
+    
+    System.out.println("====6====");
+    c3.cartDetails();
+    c1.giveDiscount(10);
+    
+    System.out.println("====7====");
+    c1.cartDetails();
+  }
+}
+
 
 public class Cart{
   public int cartNo;
@@ -253,6 +424,36 @@ public class Cart{
     
 
 8)
+public class Reader_tester {
+  public static void main(String[] args){
+    Reader r1 = new Reader();
+    Reader r2 = new Reader();
+    
+    System.out.println("1 ==========");
+    System.out.println(r1.createReader("Messi", 2));
+    System.out.println(r2.createReader("Ronaldo", 3));
+    
+    System.out.println("2 ==========");
+    r1.readerInfo();
+    
+    System.out.println("3 ==========");
+    r2.addBook("Java");
+    r2.addBook("Python");
+    r2.addBook("C++");
+    r2.readerInfo();
+    
+    System.out.println("4 ==========");
+    r1.addBook("C#");
+    r1.addBook("Rust");
+    r1.addBook("GoLang");
+    
+    System.out.println("5 ==========");
+    r2.addBook("Python");
+    
+    System.out.println("6 ==========");
+    r1.readerInfo();
+  }
+}
 
 
 public class Reader{
@@ -290,3 +491,4 @@ public class Reader{
   }
 }
   
+
