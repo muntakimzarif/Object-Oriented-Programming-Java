55c32d509d15d24e49c252cddbc94f086677712e
Md. Zarif Muntakim <78073923+muntakimzarif@users.noreply.github.com>
2025-08-02 04:13:06 +0600

    Update 02_OOP Basics, Instance Variable and Instance Method.java

--- 02_OOP Basics, Instance Variable and Instance Method.java
+++ 02_OOP Basics, Instance Variable and Instance Method.java
@@ -11,11 +11,18 @@ public class UniversityTester{
     System.out.println(u2.name);
     System.out.println(u1.country);
     System.out.println(u2.country); 
-    //Are the location of the objects the same? NO.
   }
+}
+public class University{
+    public String name;
+    public String country;
 }
 
 b)
+public class University{
+    public String name;
+    public String country;
+}
 
 public class UniversityTester{
   public static void main(String[]args){
@@ -46,6 +53,18 @@ public class Student{
   public int id = 0;
 }
 
+public class StudentTester1{
+   public static void main(String [] args){
+      Student s1 = new Student();
+      System.out.println("Name of the Student: "+s1.name);
+      System.out.println("ID of the Student: "+s1.id);
+      s1.name = "Bob";
+      s1.id = 123;
+      System.out.println("Name of the Student: "+s1.name);
+      System.out.println("ID of the Student: "+s1.id);
+   }
+}
+
 
 3)
 
@@ -53,6 +72,15 @@ public class CSECourse{
   public String courseName = "Programming Language II";
   public String courseCode = "CSE111";
   public int credit = 3;
+}
+
+public class CourseTester{
+  public static void main(String args []){
+    CSECourse c1 = new CSECourse();
+    System.out.println("Course Name: "+c1.courseName);
+    System.out.println("Course Code: "+c1.courseCode);
+    System.out.println("Credit: "+c1.credit);
+   }
 }
 
 4)
@@ -65,8 +93,39 @@ public class ImaginaryNumber{
   }
 }
 
+public class Tester6{
+  public static void main(String [] args){
+     ImaginaryNumber num1 = new ImaginaryNumber();
+     String p = num1.printNumber();
+     System.out.println(p);
+     System.out.println("1********");
+     num1.realPart=3;
+     num1.imaginaryPart=7;
+     System.out.println(num1.printNumber());
+     System.out.println("2********");
+     ImaginaryNumber num2 = new ImaginaryNumber();
+     num2.realPart=1;
+     num2.imaginaryPart=9;
+     System.out.println(num2.printNumber());
+    }
+}
 
 5)
+public class Tester5{
+  public static void main(String[] args) {
+    Course c1 = new Course();
+    Course c2 = new Course();  
+    c1.updateDetails("Programming Language I","CSE110", 3);
+    System.out.println("========== 1 ==========");
+    c1.displayCourse(); 
+    c2.updateDetails("Data Structures","CSE220", 3);
+    System.out.println("========== 2 ==========");
+    c2.displayCourse();  
+    c1.updateDetails("Programming Language II","CSE111", 3);
+    System.out.println("========== 3 ==========");
+    c1.displayCourse();
+  }
+}
 
 public class Course{
   public String courseName;
@@ -86,6 +145,29 @@ public class Course{
 
 
 6)
+public class AssignmentTester{
+  public static void main(String [] args){
+    Assignment as1 = new Assignment();
+    as1.printDetails();
+    System.out.println("1---------------");
+    as1.tasks = 11;
+    as1.difficulty = "Moderate";
+    as1.submission = true;
+    as1.printDetails();
+    System.out.println("2---------------");
+    System.out.println(as1.makeOptional());
+    System.out.println("3---------------");
+    as1.printDetails();
+    System.out.println("4---------------");
+    Assignment as2 = new Assignment();
+    as2.tasks = 12;
+    as2.difficulty = "Hard";
+    as2.submission = false;
+    as2.printDetails();
+    System.out.println("5---------------");
+    System.out.println(as2.makeOptional());
+  }
+}
 
 public class Assignment{
   public String difficulty;
@@ -109,6 +191,26 @@ public class Assignment{
 
 
 7)
+public class Tester9{
+  public static void main(String[]args){
+     CellPhone phone1 = new CellPhone();
+     phone1.printDetails();
+     phone1.model ="Nokia 1100";
+     System.out.println("1##################");
+     phone1.storeContact("Joy - 01834");
+     System.out.println("===================");
+     phone1.printDetails();
+     System.out.println("2##################");
+     phone1.storeContact("Toya - 01334");
+     phone1.storeContact("Aayan - 01135");
+     System.out.println("===================");
+     phone1.printDetails();
+     System.out.println("3##################");
+     phone1.storeContact("Sani - 01441");
+     System.out.println("===================");
+     phone1.printDetails();  
+  }
+}
 
 public class CellPhone{
   public String model = "unknown";
@@ -138,6 +240,38 @@ public class CellPhone{
 
 
 8)
+public class Tester3{
+  public static void main(String[] args){
+
+    Employee emp1 = new Employee();
+    Employee emp2 = new Employee();
+    Employee emp3 = new Employee();
+      
+    emp1.newEmployee("Harry Potter");
+    emp2.newEmployee("Hermione Granger");
+    emp3.newEmployee("Ron Weasley");
+    System.out.println("1 ==========");
+    emp1.displayInfo();
+    System.out.println("2 ==========");
+    emp2.displayInfo();
+    System.out.println("3 ==========");
+    emp3.displayInfo();
+    System.out.println("4 ==========");
+    emp1.calculateTax();
+    System.out.println("5 ==========");
+    emp1.promoteEmployee("lead");
+    System.out.println("6 ==========");
+    emp1.calculateTax();
+    System.out.println("7 ==========");
+    emp1.displayInfo();
+    System.out.println("8 ==========");
+    emp3.promoteEmployee("manager");
+    System.out.println("9 ==========");
+    emp3.calculateTax();
+    System.out.println("10 ==========");
+    emp3.displayInfo();
+  }
+}
 
 public class Employee 
 {
@@ -204,6 +338,7 @@ public class Employee
     System.out.println("New Salary: " + salary + " Tk");
   }
 }
+
 
 
 
